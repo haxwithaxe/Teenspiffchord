7d92c5c884926160b430c889717b3c7cbacc3c0c
haxwithaxe <me@haxwithaxe.net>
2016-11-13 16:10:09 -0500

    works as expected so far.

--- Teenspiffchord.ino
+++ Teenspiffchord.ino
@@ -1,14 +1,27 @@
-//#include <Versalino.h>
-
-// teenspiffchord v1 
+// teenspiffchord v2
 // - reinvented spiffchorder
-// - ment for teensy 2.0 (atmega32u4) but might work on others
+// - redesigned for teensy 3.2 original worked on teensy 2.0
 
 #include <Bounce.h>
 #include "nasa_us.h"
 
+/* uncomment to enable debug output
+#define DEBUG
+*/
+
+// Magic keyboard numbers
+#define FUNCTION_MODE 1
+#define NUMBER_MODE 2
+#define DEFAULT_MODE 3
+#define SHIFT 1
+
+// Debounce interval
 #define DEBOUNCE_TIMEOUT 10
-#define TYPETIME 500          // 500 for noooooobs
+
+// Time to wait before assuming the chord has been completely pressed.
+#define TYPETIME 50
+
+// Pins
 #define BUTTON_F 2
 #define BUTTON_C 3
 #define BUTTON_N 4
@@ -18,6 +31,19 @@
 #define BUTTON_P 18
 #define LEDPIN 13
 
+
+unsigned long presstime;
+boolean pressed;
+int chordvalue;
+int last_chordvalue = 0;
+int keymapmode;
+int pressedkey;
+int shiftmode;
+int buttonsheld;
+int last_buttonsheld = 0;
+int currentmodifier;
+bool repeat_mode = false;
+
 Bounce button_p = Bounce(BUTTON_P, DEBOUNCE_TIMEOUT);
 Bounce button_r = Bounce(BUTTON_R, DEBOUNCE_TIMEOUT);
 Bounce button_m = Bounce(BUTTON_M, DEBOUNCE_TIMEOUT);
@@ -26,20 +52,29 @@ Bounce button_n = Bounce(BUTTON_N, DEBOUNCE_TIMEOUT);
 Bounce button_c = Bounce(BUTTON_C, DEBOUNCE_TIMEOUT);
 Bounce button_f = Bounce(BUTTON_F, DEBOUNCE_TIMEOUT);
 
-unsigned long presstime;
-boolean pressed;
-int chordvalue;
-int keymapmode;
-int pressedkey;
-int shiftmode;
-int buttonsheld;
-int currentmodifier;
 
-#define FUNCTION_MODE 1
-#define NUMBER_MODE 2
-#define DEFAULT_MODE 3
+void debug(char*name) {
+#ifdef DEBUG
+	Serial.print(String(name));
+	Serial.print(": "+String(pressed));
+	Serial.print(", buttonsheld = "+String(buttonsheld, DEC));
+	Serial.print(", last buttonsheld = "+String(last_buttonsheld, DEC));
+	Serial.print(", chordvalue = "+String(chordvalue, DEC));
+	Serial.print(", last_chordvalue = "+String(last_chordvalue, DEC));
+	Serial.println();
+#endif
+}
 
-#define SHIFT 1
+/*
+   Bypass checks if in repeat mode for rapid 
+   keypresses while holing down keys
+
+   repeat mode not implemented but the bypasses required are in place.
+ */
+bool in_repeat_mode(bool condition) {
+	if (repeat_mode) return true;
+	return condition;
+}
 
 int getkey(int value, int keymap) {
 	int key;
@@ -116,58 +151,71 @@ void processchord(int value) {
   pressedkey = getkey(value,keymapmode);
       
 	switch (pressedkey) {
+
 		case MODE_FUNC:
 			// set function mode
 			keymapmode = FUNCTION_MODE;
 			break;
+
 		case  MODE_NUM:
 			// set number mode
 			keymapmode = NUMBER_MODE;
 			break;
+
 		case  MODE_RESET:
 			// set mode back to default mode
 			keymapmode = DEFAULT_MODE;
 			break;
+
 		case  SHIFTMODE:
 			// pressed shift
 			shiftmode = SHIFT;
 			currentmodifier = (currentmodifier | MODIFIERKEY_SHIFT);
 			break;
+
 		case  ALTSHIFTMODE:
 			// pressed alt
 			shiftmode = SHIFT;
 			currentmodifier = (currentmodifier | MODIFIERKEY_ALT);
 			break;
+
 		case  CTRLSHIFTMODE:
 			// pressed ctrl
 			shiftmode = SHIFT;
 			currentmodifier = (currentmodifier | MODIFIERKEY_CTRL);
 			break;
+
 		case  NUMSHIFTMODE:
 			// pressed numshiftmode ( N+C )
 			shiftmode = SHIFT;
 			currentmodifier = (currentmodifier | MODIFIERKEY_SHIFT);
 			keymapmode = NUMBER_MODE;
 			break;
+
 		case  MACRO_quotes:
 			macro(KEY_QUOTE, KEY_QUOTE, KEY_LEFT);
 			break;
+
 		case  MACRO_00:
 			macro(KEY_0, KEY_0);
 			break;
+
 		case MACRO_000:
 			macro(KEY_0, KEY_0, KEY_0);
 			break;
+
 		case  MACRO_parens:
 			shiftmode = SHIFT;
 			macro(KEY_9, KEY_0);
 			send_key(KEY_LEFT);
 			break;
+
 		default:
 			// Normal chord, just print the output :D
 			send_key(pressedkey);
 			keymapmode = DEFAULT_MODE;
 			break;
+
 	}
 }
 
@@ -182,9 +230,14 @@ void releasekeys() {
 	Keyboard.send_now();
 }
 
+
 void setup() {
+
 	Serial.begin(9600);
+	pinMode(LEDPIN, OUTPUT);
+
 	pinMode(0, INPUT_PULLUP);
+
 	pinMode(BUTTON_F, INPUT_PULLUP);
 	pinMode(BUTTON_C, INPUT_PULLUP);
 	pinMode(BUTTON_N, INPUT_PULLUP);
@@ -192,8 +245,8 @@ void setup() {
 	pinMode(BUTTON_M, INPUT_PULLUP);
 	pinMode(BUTTON_R, INPUT_PULLUP);
 	pinMode(BUTTON_P, INPUT_PULLUP);
-	pinMode(LEDPIN, OUTPUT);
 }
+
 
 void loop() {
 	button_p.update();
@@ -208,99 +261,93 @@ void loop() {
 		if (chordvalue) { 
 			releasekeys(); 
 		}
-		chordvalue = 0; 
-		buttonsheld = 0; 
 		digitalWrite(LEDPIN, LOW);
 	}
+	buttonsheld = 0; 
+	chordvalue = 0; 
 
-	if (button_p.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("pinky");
-			pressed=true;
-			presstime=millis();
-		}
+	// Track the buttons that are currently pressed.
+	if (button_p.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 1;
 	}
-	if (button_r.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("ring");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_r.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 2;
 	}
-	if (button_m.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("middle");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_m.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 4;
 	}
-	if (button_i.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("index");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_i.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 8;
 	}
-	if (button_n.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("near");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_n.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 16;
 	}
-	if (button_c.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("center");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_c.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 32;
 	}
-	if (button_f.fallingEdge()) {
-		if (!pressed) {
-			Serial.println("far");
-			pressed=true;
-			presstime=millis();
-		}
+
+	if (button_f.read() == LOW) {
+		pressed=true;
 		buttonsheld++;
 		chordvalue += 64;
 	}
+	
+	// Start the timer for any new keypress.
+	if (button_p.fallingEdge()) {
+			debug("+ pinky");
+			presstime=millis();
+	}
 
-	if (button_p.risingEdge()) {
-		buttonsheld--;
+	if (button_r.fallingEdge()) {
+			debug("+ ring");
+			presstime=millis();
 	}
-	if (button_r.risingEdge()) {
-		buttonsheld--;
+
+	if (button_m.fallingEdge()) {
+			debug("+ middle");
+			presstime=millis();
 	}
-	if (button_m.risingEdge()) {
-		buttonsheld--;
+
+	if (button_i.fallingEdge()) {
+			debug("+ index");
+			presstime=millis();
 	}
-	if (button_i.risingEdge()) {
-		buttonsheld--;
+
+	if (button_n.fallingEdge()) {
+			debug("+ near");
+			presstime=millis();
 	}
-	if (button_n.risingEdge()) {
-		buttonsheld--;
-		}
-	if (button_c.risingEdge()) {
-		buttonsheld--;
+
+	if (button_c.fallingEdge()) {
+			debug("+ center");
+			presstime=millis();
 	}
-	if (button_f.risingEdge()) {
-		buttonsheld--;
+
+	if (button_f.fallingEdge()) {
+			debug("+ far");
+			presstime=millis();
 	}
 
 	if (buttonsheld == 0) {
 		pressed = false;
+		last_chordvalue = 0;
 	}
 
 	if (pressed) {
@@ -308,13 +355,24 @@ void loop() {
 	}
 
 	if (chordvalue) {
-		if ((presstime + TYPETIME) < millis()) {
-			processchord(chordvalue); 
+		bool is_new_chordvalue;
+		if (chordvalue > last_chordvalue) {
+			is_new_chordvalue = true;
+		} else {
+			is_new_chordvalue = false;
+			last_buttonsheld = buttonsheld;
+			last_chordvalue = chordvalue;
+		}
+
+		if ((presstime + TYPETIME) < millis() &&  in_repeat_mode(is_new_chordvalue)) {
+			processchord(chordvalue);
+			last_chordvalue = chordvalue;
+			last_buttonsheld = buttonsheld;
 		} else if (buttonsheld == 0) {
 			processchord(chordvalue);
-			pressed = false;      
+			pressed = false;
+			last_chordvalue = 0;
 		}
 	}
-
 }
 
